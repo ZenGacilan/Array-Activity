9320906412c3fa24070a545c0cb66969ea70a2c7
ZenGacilan <potapetekau@gmail.com>
2024-10-13 21:29:27 +0800

    initial commit

---
+++ add.js
@@ -0,0 +1,16 @@
+// Create an array called favSingers and store your three favorite singers
+let favSingers = ["Taylor Swift", "Adele", "Beyoncé"];
+
+// Log the first singer in the array using array index notation
+console.log(favSingers[0]);
+
+// Create an array called favNumbers and store four of your favorite numbers
+let favNumbers = [7, 13, 21, 37];
+
+// Create an array named mixedArr and store different data types
+let mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(mixedArr[0]); 
+console.log(mixedArr[1]); 
+console.log(mixedArr[2]); 
+console.log(mixedArr[3]); 
